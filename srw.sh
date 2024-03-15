fef2cab4842c655ad7d5e0d0d3bfdbcf6997f626
Natalie Perlin <Natalie.Perlin@noaa.gov>
2024-03-15 14:22:56 +0000

    update srw.sh, binaries in ./container-bin

--- srw.sh
+++ srw.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export SINGULARITYENV_FI_PROVIDER=tcp
 export SINGULARITY_SHELL=/usr/share/lmod/lmod/init/bash
-export SINGULARITYENV_PREPEND_PATH="/lustre/ufs-srweather-app/bin"
+export SINGULARITYENV_PREPEND_PATH="/lustre/ufs-srweather-app/container-bin"
 img="/lustre/ubuntu20.04-intel-srwapp-release-public-v2.2.0.img"
 cmd=$(basename "$0")
 arg="$@"

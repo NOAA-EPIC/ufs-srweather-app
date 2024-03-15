478d9adc20f13cf27ead651e0ff595ab30a8fbd2
Bruce Kropp - Raytheon <104453151+BruceKropp-Raytheon@users.noreply.github.com>
2024-03-15 14:26:52 -0700

    Update srw_build.sh
    memMax on a separate line in JSON

--- .cicd/scripts/srw_build.sh
+++ .cicd/scripts/srw_build.sh
@@ -27,7 +27,7 @@ fi
 # Build and install
 cd ${workspace}/tests
 set +e
-/usr/bin/time -p -f '{\n  "cpu": "%P"\n, "memMax": "%M", "mem": {"text": "%X", "data": "%D", "swaps": "%W", "context": "%c", "waits": "%w"}\n, "pagefaults": {"major": "%F", "minor": "%R"}\n, "filesystem": {"inputs": "%I", "outputs": "%O"}\n, "time": {"real": "%e", "user": "%U", "sys": "%S"}\n}' -o ${WORKSPACE}/${SRW_PLATFORM}-${SRW_COMPILER}-time-srw_build.json \
+/usr/bin/time -p -f '{\n  "cpu": "%P"\n, "memMax": "%M"\n, "mem": {"text": "%X", "data": "%D", "swaps": "%W", "context": "%c", "waits": "%w"}\n, "pagefaults": {"major": "%F", "minor": "%R"}\n, "filesystem": {"inputs": "%I", "outputs": "%O"}\n, "time": {"real": "%e", "user": "%U", "sys": "%S"}\n}' -o ${WORKSPACE}/${SRW_PLATFORM}-${SRW_COMPILER}-time-srw_build.json \
     ./build.sh ${platform} ${SRW_COMPILER}
 build_exit=$?
 set -e
